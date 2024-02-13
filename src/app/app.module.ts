9d107a09a3fd1c16d3aadded1a702608d2713d41
AbijitE <eabijit746@gmail.com>
2024-02-13 18:01:26 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LiveComponent } from './live/live.component';
+import { PointTableComponent } from './point-table/point-table.component';
+import { HomeComponent } from './home/home.component';
+import { HistoryComponent } from './history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LiveComponent,
+    PointTableComponent,
+    HomeComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
